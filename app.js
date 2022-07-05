469fe80653206eef0158e4484b76069c0b5ccfb0
JuanLicciardi <juanelicciardi@gmail.com>
2022-07-05 10:51:10 -0300

    Maquetacion de Home e inicio de estilos

--- app.js
+++ app.js
@@ -9,5 +9,8 @@ const app = express();
 app.use(express.static('public'));
 
 app.get('/', (req, res) => res.sendFile(path.resolve(__dirname,'views','index.html')));
+app.get('/about', (req, res) => res.sendFile(path.resolve(__dirname,'views','about.html')));
+app.get('/music', (req, res) => res.sendFile(path.resolve(__dirname,'views','music.html')));
+app.get('/contact', (req, res) => res.sendFile(path.resolve(__dirname,'views','contact.html')));
 
 app.listen(port, () => console.log('Server run in http://localhost:' + port));
